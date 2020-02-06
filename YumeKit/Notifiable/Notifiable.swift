0d76f36d8edecf44d0fd72b6beaf75fd87332087
Yume <yume190@gmail.com>
2020-02-06 09:57:02 +0800

    Update Notifiable.swift

--- YumeKit/Notifiable/Notifiable.swift
+++ YumeKit/Notifiable/Notifiable.swift
@@ -8,24 +8,49 @@
 
 import Foundation
 
+public protocol Cancelable {
+    func cancel()
+}
+
+public protocol NotificationCancelable {}
+
+fileprivate final class NotificationObserver: NotificationCancelable {
+    
+    private let object: NSObjectProtocol
+    fileprivate init(object: NSObjectProtocol) {
+        self.object = object
+    }
+    
+    public func cancel() {
+        NotificationCenter.default.removeObserver(self.object)
+    }
+}
+
 public protocol Notifiable {
     var name: NSNotification.Name { get }
 
+    func add(object: Any?, queue: OperationQueue?, using: @escaping (Notification) -> Void) -> NotificationCancelable
     func add(observer: Any, selector: Selector, object: Any?)
     func remove(observer: Any, object: Any?)
     func post(object: Any?, userInfo: [AnyHashable: Any]?)
 }
 
-extension Notifiable {
-    public func add(observer: Any, selector: Selector, object: Any? = nil) {
+public extension Notifiable {
+    func add(object: Any? = nil, queue: OperationQueue?, using: @escaping (Notification) -> Void) -> NotificationCancelable {
+        return NotificationObserver(
+            object: NotificationCenter.default.addObserver(forName: self.name, object: object, queue: queue, using: using)
+        )
+    }
+    
+    func add(observer: Any, selector: Selector, object: Any? = nil) {
         NotificationCenter.default.addObserver(observer, selector: selector, name: name, object: object)
     }
 
-    public func remove(observer: Any, object: Any? = nil) {
+    func remove(observer: Any, object: Any? = nil) {
         NotificationCenter.default.removeObserver(observer, name: name, object: object)
     }
 
-    public func post(object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
+    func post(object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
         NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
     }
 }
